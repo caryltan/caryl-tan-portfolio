7df5da3dc56421c8535da92fe3178112d417826d
Caryl Tan <tan.caryl@gmail.com>
2023-01-26 22:38:29 -0500

    hamburger menu change to X and listener for it

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 //mobile hamburger menu
 const hamburgerIcon = document.getElementById("hamburger-icon");
 const mobileMenu = document.querySelector(".mobile-menu");
+const lineOne = document.querySelector(".line-one");
+console.log(lineOne);
+const lineTwo = document.querySelector(".line-two");
+const lineThree = document.querySelector(".line-three");
 
 hamburgerIcon.addEventListener('click', () => {
     mobileMenu.classList.toggle("open");
-    console.log(mobileMenu)
+    lineOne.classList.toggle("hamburger-animate");
+    lineTwo.classList.toggle("hamburger-animate");
+    lineThree.classList.toggle("hamburger-animate");
 })
